e65fe6b732b97aa677ab53e2ac53fabfb46e8cc2
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2021-05-04 11:06:24 -0400

    Update pipeline example

--- examples/pipelines.go
+++ examples/pipelines.go
@@ -4,46 +4,46 @@
 package main
 
 import (
-	"fmt"
-	"txtai.go"
+    "fmt"
+    "github.com/neuml/txtai.go"
 )
 
 func main() {
-	service := "http://localhost:8000"
-	segment := txtai.Segmentation(service)
+    service := "http://localhost:8000"
+    segment := txtai.Segmentation(service)
 
-	sentences := "This is a test. And another test."
+    sentences := "This is a test. And another test."
 
-	fmt.Println("---- Segmented Text ----")
-	fmt.Println(segment.Segment(sentences))
+    fmt.Println("---- Segmented Text ----")
+    fmt.Println(segment.Segment(sentences))
 
-	textractor := txtai.Textractor(service)
-	text := textractor.Textract("/tmp/txtai/article.pdf");
+    textractor := txtai.Textractor(service)
+    text := textractor.Textract("/tmp/txtai/article.pdf");
 
-	fmt.Println("\n---- Extracted Text ----")
-	fmt.Println(text)
+    fmt.Println("\n---- Extracted Text ----")
+    fmt.Println(text)
 
-	summary := txtai.Summary(service)
-	summarytext :=  summary.Summary(text.(string), -1, -1)
+    summary := txtai.Summary(service)
+    summarytext :=  summary.Summary(text.(string), -1, -1)
 
-	fmt.Println("\n---- Summary Text ----")
-	fmt.Println(summarytext)
+    fmt.Println("\n---- Summary Text ----")
+    fmt.Println(summarytext)
 
-	translate := txtai.Translation(service)
-	translation := translate.Translate(summarytext.(string), "es", "")
+    translate := txtai.Translation(service)
+    translation := translate.Translate(summarytext.(string), "es", "")
 
-	fmt.Println("\n---- Summary Text in Spanish ----")
-	fmt.Println(translation)
+    fmt.Println("\n---- Summary Text in Spanish ----")
+    fmt.Println(translation)
 
-	workflow := txtai.Workflow(service)
-	output := workflow.Workflow("sumspanish", []string {text.(string)})
+    workflow := txtai.Workflow(service)
+    output := workflow.Workflow("sumspanish", []string {text.(string)})
 
-	fmt.Println("\n---- Workflow [Extract Text->Summarize->Translate] ----")
-	fmt.Println(output)
+    fmt.Println("\n---- Workflow [Extract Text->Summarize->Translate] ----")
+    fmt.Println(output)
 
-	transcribe := txtai.Transcription(service)
-	transcription := transcribe.Transcribe("/tmp/txtai/Make_huge_profits.wav")
+    transcribe := txtai.Transcription(service)
+    transcription := transcribe.Transcribe("/tmp/txtai/Make_huge_profits.wav")
 
-	fmt.Println("\n---- Transcribed Text ----")
-	fmt.Println(transcription)
+    fmt.Println("\n---- Transcribed Text ----")
+    fmt.Println(transcription)
 }
